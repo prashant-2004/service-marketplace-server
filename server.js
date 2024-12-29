7803782c9d32a51a3d7281028e2402b688490d63
Prashant Kale <127956232+prashant-2004@users.noreply.github.com>
2024-12-29 17:53:43 +0530

    Update server.js

--- server.js
+++ server.js
@@ -18,10 +18,7 @@ app.use(express.json());
 // cookie-parser middleware
 app.use(cookieParser()); 
 app.use(
-  cors({
-    origin: "http://localhost:3000",
-    credentials: true,
-  })
+  cors()
 );
 
 // Rout Link
